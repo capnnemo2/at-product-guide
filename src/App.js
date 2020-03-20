ff89006a411bbe977fcb41845efe4f8e13a71971
Ben Hernandez <benjammin234@gmail.com>
2020-03-20 10:39:12 -0700

    Creates header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header/Header";
 
 export default class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="App">
+        <header>
+          <Header />
+        </header>
+      </div>
+    );
   }
 }
