fcb9aa3c1a7612042381532f53d77ac95b3c64de
Ben Hernandez <benjammin234@gmail.com>
2020-03-26 11:22:45 -0700

    Begins creating context

--- src/App.js
+++ src/App.js
@@ -12,36 +12,46 @@ import EditProduct from "./EditProduct/EditProduct";
 import EditComment from "./EditComment/EditComment";
 import NotFound from "./NotFound/NotFound";
 import "./App.css";
+import ATContext from "./ATContext";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <nav>
-          <Nav />
-        </nav>
-        <header>
-          {/* maybe just want the header for the landing and home page, when you look at individual products maybe just a navbar? */}
-          <Header />
-        </header>
+  state = {
+    products: []
+  };
 
-        <main>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route path="/home" component={Home} />
-            <Route path="/resources" component={Resources} />
-            <Route
-              path="/productDetails/:product_id"
-              component={ProductDetails}
-            />
-            <Route path="/add-product" component={AddProduct} />
-            <Route path="/add-comment" component={AddComment} />
-            <Route path="/edit-product" component={EditProduct} />
-            <Route path="/edit-comment" component={EditComment} />
-            <Route component={NotFound} />
-          </Switch>
-        </main>
-      </div>
+  render() {
+    const value = {
+      products: this.state.products
+    };
+    return (
+      <ATContext.Provider value={value}>
+        <div className="App">
+          <nav>
+            <Nav />
+          </nav>
+          <header>
+            {/* maybe just want the header for the landing and home page, when you look at individual products maybe just a navbar? */}
+            <Header />
+          </header>
+
+          <main>
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route path="/home" component={Home} />
+              <Route path="/resources" component={Resources} />
+              <Route
+                path="/productDetails/:product_id"
+                component={ProductDetails}
+              />
+              <Route path="/add-product" component={AddProduct} />
+              <Route path="/add-comment" component={AddComment} />
+              <Route path="/edit-product" component={EditProduct} />
+              <Route path="/edit-comment" component={EditComment} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
+        </div>
+      </ATContext.Provider>
     );
   }
 }
