6c0c87029507c4f2a7c2b3bc5a7d4aaf936a5037
Ben Hernandez <benjammin234@gmail.com>
2020-04-13 09:41:37 -0700

    Adjusts the home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,23 +6,28 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <section className="section">
-          <p>
-            This product guide is a repository of specifications on how to make
-            Affable Triceratops products.
-          </p>
-        </section>
-        <section className="section">
-          <p>
-            Check out pictures, measurements, and detailed instructions on what
-            to do, when to do it, and tips for execution.
-          </p>
-        </section>
-        <section className="section">
-          <p>
-            Post comments to share your personal tricks, see what others do.
-          </p>
+        <section className="first">
+          <div className="fake-img"></div>
+          <p>Learn how to weld trellises, arbors, and topiary.</p>
           <Link to="/products">Begin</Link>
+        </section>
+
+        <section className="section">
+          <p>
+            Peruse the entire repository of specifications. Search for specific
+            products.
+          </p>
+        </section>
+        <section className="section">
+          <p>
+            Double check measurements, reference images, study detailed
+            instructions.
+          </p>
+        </section>
+        <section className="section">
+          <p>
+            Share your personal tricks, get advice from other professionals.
+          </p>
         </section>
       </div>
     );
