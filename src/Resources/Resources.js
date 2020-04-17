910fefce9f72d48e6dc3b0e4f0b0f9f5cc6b5ebd
Ben Hernandez <benjammin234@gmail.com>
2020-04-17 08:33:40 -0700

    Adds an about section to resources

--- src/Resources/Resources.js
+++ src/Resources/Resources.js
@@ -5,55 +5,49 @@ export default class Resources extends React.Component {
   render() {
     return (
       <div className="Resources">
-        {/* <section>
-          <p>
-            This page will include images and descriptions of the various jigs
-            and benders. It will also include general tips and guidelines. For
-            example: All arbors do not require additional foot spikes.
-          </p>
-          <p>
-            Perhaps this section will also include things like how to make top
-            rings?
-          </p>
-        </section> */}
         <section>
-          <h2>General Guidance</h2>
-          <p>
-            For each product, the materials listed are for one of that product.
-            However, you will encounter directions in sections like prep bend
-            and prep weld that give guidance for putting together batches.
+          <h2>About</h2>
+          <p className="text-block">
+            The content of this application applies to a specific company that
+            fabricates and distributes trellises, arbors, topiary, and more to
+            garden nurseries across the country. However, the structure of the
+            application could be applied to any business that manufactures
+            products which require detailed instructions. A repository of
+            specifications is important so that hard-earned knowledge isn't lost
+            during any transitions or changes the company may undergo.
           </p>
         </section>
         <section>
-          <h2>Arbors</h2>
-          <p>
-            Arbors do not get extra foot spikes. The bent frame pieces provide
-            the only foot spikes.
+          <h2>All Products</h2>
+          <p className="text-block">
+            For each product, the materials listed are for a single product.
+            However, you will encounter directions in prep bend and prep weld
+            that give guidance for putting together batches to maximize
+            efficiency.
           </p>
-          <p>Cutting arbor mesh: mesh is open on all sides.</p>
-          <p>Arbor mesh gets welded on backwards from flat work.</p>
         </section>
+
         <section>
           <h2>Trellises</h2>
-          <p>
+          <p className="text-block">
             Trellis mesh measurements are provided with the height x width. When
             you cut the mesh, the width of the mesh is open on both sides, open
             on the top, but not the bottom.
           </p>
-          <p>
+          <p className="text-block">
             Generally, hard 3/8" cross pieces are 1" narrower than the width of
             the trellis/mesh (IE: FT51 requires 2 x 11"), but depending on your
             mess cutting style you may have to adjust your measurements.
           </p>
-          <p>
+          <p className="text-block">
             Back weld all 3/8" connections. Products like DFTs and BFTs also
             have 1/4" - 3/8" connections that also need to be back welded.
           </p>
-          <p>
+          <p className="text-block">
             Trellises that are wider than 1' require additional foot spikes.
             Trellises between 1' and 4' wide get one extra foot spike.
           </p>
-          <ul>
+          <ul className="trellis-feet">
             <li>1' = no extra foot spikes</li>
             <li>1' &lt; trellis &lt; 4' = one extra foot spike, centered</li>
             <li>
@@ -69,6 +63,20 @@ export default class Resources extends React.Component {
               stable.
             </li>
           </ul>
+        </section>
+
+        <section>
+          <h2>Arbors</h2>
+          <p className="text-block">
+            Arbors do not get extra foot spikes. The bent frame pieces provide
+            the only foot spikes.
+          </p>
+          <p className="text-block">
+            Cutting arbor mesh: mesh is open on all sides.
+          </p>
+          <p className="text-block">
+            Arbor mesh gets welded on backwards from flat work.
+          </p>
         </section>
       </div>
     );
